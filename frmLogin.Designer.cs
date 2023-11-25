ae0df4c6f53ea29f4a2241f059cba93df8b992d5
Tonii2 <151387700+Tonii2@users.noreply.github.com>
2023-11-25 19:48:41 -0400

    Add files via upload

---
+++ frmLogin.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Ejemplo_Login
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_intentos = new System.Windows.Forms.Label();
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.btn_ingresar = new System.Windows.Forms.Button();
+            this.txt_clave = new System.Windows.Forms.TextBox();
+            this.txt_usuario = new System.Windows.Forms.TextBox();
+            this.lbl_clave = new System.Windows.Forms.Label();
+            this.lbl_usuario = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_intentos
+            // 
+            this.lbl_intentos.AutoSize = true;
+            this.lbl_intentos.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_intentos.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_intentos.Location = new System.Drawing.Point(89, 108);
+            this.lbl_intentos.Name = "lbl_intentos";
+            this.lbl_intentos.Size = new System.Drawing.Size(123, 16);
+            this.lbl_intentos.TabIndex = 17;
+            this.lbl_intentos.Text = "Numero de intentos";
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_salir.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_salir.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_salir.Location = new System.Drawing.Point(311, 340);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(178, 58);
+            this.btn_salir.TabIndex = 15;
+            this.btn_salir.Text = "Salir";
+            this.btn_salir.UseVisualStyleBackColor = false;
+            this.btn_salir.Click += new System.EventHandler(this.btn_salir_Click);
+            // 
+            // btn_ingresar
+            // 
+            this.btn_ingresar.BackColor = System.Drawing.Color.Green;
+            this.btn_ingresar.Enabled = false;
+            this.btn_ingresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ingresar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_ingresar.Location = new System.Drawing.Point(70, 340);
+            this.btn_ingresar.Name = "btn_ingresar";
+            this.btn_ingresar.Size = new System.Drawing.Size(178, 58);
+            this.btn_ingresar.TabIndex = 14;
+            this.btn_ingresar.Text = "Ingresar";
+            this.btn_ingresar.UseVisualStyleBackColor = false;
+            this.btn_ingresar.Click += new System.EventHandler(this.btn_ingresar_Click);
+            // 
+            // txt_clave
+            // 
+            this.txt_clave.Location = new System.Drawing.Point(269, 228);
+            this.txt_clave.Name = "txt_clave";
+            this.txt_clave.PasswordChar = '*';
+            this.txt_clave.Size = new System.Drawing.Size(142, 22);
+            this.txt_clave.TabIndex = 13;
+            this.txt_clave.Tag = "";
+            // 
+            // txt_usuario
+            // 
+            this.txt_usuario.Location = new System.Drawing.Point(269, 168);
+            this.txt_usuario.Name = "txt_usuario";
+            this.txt_usuario.Size = new System.Drawing.Size(165, 22);
+            this.txt_usuario.TabIndex = 12;
+            this.txt_usuario.TextChanged += new System.EventHandler(this.txt_usuario_TextChanged);
+            // 
+            // lbl_clave
+            // 
+            this.lbl_clave.AutoSize = true;
+            this.lbl_clave.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_clave.Location = new System.Drawing.Point(93, 228);
+            this.lbl_clave.Name = "lbl_clave";
+            this.lbl_clave.Size = new System.Drawing.Size(91, 29);
+            this.lbl_clave.TabIndex = 11;
+            this.lbl_clave.Text = "Clave:";
+            // 
+            // lbl_usuario
+            // 
+            this.lbl_usuario.AutoSize = true;
+            this.lbl_usuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_usuario.Location = new System.Drawing.Point(90, 168);
+            this.lbl_usuario.Name = "lbl_usuario";
+            this.lbl_usuario.Size = new System.Drawing.Size(115, 29);
+            this.lbl_usuario.TabIndex = 10;
+            this.lbl_usuario.Text = "Usuario:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft PhagsPa", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(170, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 33);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Formulario de logeo";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Ejemplo_Login.Properties.Resources.not_visible_interface_symbol_of_an_eye_with_a_slash_on_it_icon_icons_com_57783;
+            this.pictureBox1.Location = new System.Drawing.Point(403, 228);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(31, 22);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_2);
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            this.pictureBox1.MouseHover += new System.EventHandler(this.pictureBox1_MouseHover);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox2.Image = global::Ejemplo_Login.Properties.Resources.callesespacio;
+            this.pictureBox2.Location = new System.Drawing.Point(-116, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1104, 596);
+            this.pictureBox2.TabIndex = 18;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 583);
+            this.ControlBox = false;
+            this.Controls.Add(this.lbl_intentos);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_salir);
+            this.Controls.Add(this.btn_ingresar);
+            this.Controls.Add(this.txt_clave);
+            this.Controls.Add(this.txt_usuario);
+            this.Controls.Add(this.lbl_clave);
+            this.Controls.Add(this.lbl_usuario);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "frmLogin";
+            this.Text = "Login";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_intentos;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_salir;
+        private System.Windows.Forms.Button btn_ingresar;
+        private System.Windows.Forms.TextBox txt_clave;
+        private System.Windows.Forms.TextBox txt_usuario;
+        private System.Windows.Forms.Label lbl_clave;
+        private System.Windows.Forms.Label lbl_usuario;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
---
+++ frmLogin.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Ejemplo_Login
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_ingresar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                // crear una conexion
+                SqlConnection mi_conexion = new SqlConnection(@"Data Source=LAPTOP-RHMTR2G9\SQLEXPRESS; Initial Catalog=probd; Integrated Security=True");
+                // abrir una conexion
+                mi_conexion.Open();
+                //comando de consulta a la tabla de usuarios
+                SqlCommand consulta = new SqlCommand("select nombre, clave, rol from usuario where nombre = '" + txt_usuario.Text + "' And clave = '" + txt_clave.Text + "' ", mi_conexion);
+
+                // se ejecuta el comando de consulta
+
+                consulta.ExecuteNonQuery();
+
+                DataSet ds = new DataSet();
+
+                SqlDataAdapter da = new SqlDataAdapter(consulta);
+
+                da.Fill(ds, "usuario");
+
+                DataRow dr;
+                dr = ds.Tables["usuario"].Rows[0];
+
+                if (txt_usuario.Text == dr["nombre"].ToString() || txt_clave.Text == dr["clave"].ToString())
+                {
+
+                    //instanciando el formulario principal
+                    Principal frmPrincipal = new Principal();
+                    frmPrincipal.Show();//abriendo el formulario principal
+                    Hide();//esto sirve para ocultar el formulario de login
+
+                }
+                else
+                {
+                     MessageBox.Show("el usuario " + txt_usuario.Text +"---> " + dr["nombre"].ToString()+ " -- " +  txt_clave.Text + " --> "+ dr["clave"].ToString());
+                }
+
+                mi_conexion.Close();
+            }
+            catch
+            {
+                //en caso que la contraseña sea erronea mostrara un mensaje
+                //dentro de los parentesis va: "Mensaje a mostrar","Titulo de la ventana",botones a mostrar en ste caso OK, icono a mostrar en este caso uno de error
+                //MessageBox.Show("select nombre, clave, rol from usuarios where nombre = '" + txt_usuario.Text + "' And contraseña = '" + txt_clave.Text + "' ");
+
+                MessageBox.Show("Error! Su contraseña y/o usuario son invalidos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        private void btn_salir_Click(object sender, EventArgs e)
+        {
+            //Salir de la aplicacion
+            Application.Exit();
+        }
+
+
+        private void txt_usuario_TextChanged(object sender, EventArgs e)
+        {
+            btn_ingresar.Enabled = true;
+        }
+
+        private void pictureBox1_MouseHover(object sender, EventArgs e)
+        {
+            txt_clave.PasswordChar = '\0';
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            txt_clave.PasswordChar = '*';
+        }
+
+        private void lbl_intentos_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Listarpersonas()
+        {
+
+        }
+
+        private void pictureBox1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
